501d8f6c0d64097ce8e498b889bfd0cdd4dbdccf
Ben Garski <blgarski@uwm.edu>
2020-02-14 17:15:14 -0600

    Added Game-Coding Column

--- app.R
+++ app.R
@@ -76,11 +76,19 @@ server <- function(input, output) {
         dfCombined <- data.frame()
         
         for (i in 1:length(input$files$name)) {
+            # Read File
             df1 <- read.csv(input$files$datapath[i], skip = 9)
+            
+            # Get Date From File Name
             df1$date <- str_extract(readLines(input$files$datapath[i], n=1), "\\d+/\\d+/\\d+")
+            df1$date <- as.Date(df1$date, "%m/%d/%Y")
+            
+            # Get Activity Type From File Name
             df1$activity <- str_extract(input$files$name[i], "practice|Practice|Activity|Game")
             df1$activity[df1$activity == "Activity"] <- "practice"
             df1$activity <- str_to_title(df1$activity)
+            
+            # Filter and Combine
             df1 <- df1 %>% filter(Period.Name == "Session") %>% select(1,4,7:9,16, date, activity)
             dfCombined <- rbind(df1, dfCombined)
             
