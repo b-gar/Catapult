5552c5d409fdfc79e7e639738748dffa242afa13
Ben Garski <blgarski@uwm.edu>
2020-02-17 15:08:41 -0600

    Added Plotly Graph for Average Player Load by Game Code

--- app.R
+++ app.R
@@ -1,12 +1,19 @@
 library(shiny)
 library(shinydashboard)
 library(shinycssloaders)
+library(shinyjs)
 library(DT)
 library(dplyr)
 library(data.table)
 library(stringr)
 library(plotly)
 library(ggplot2)
+
+# Specify CSS For Error Messsage
+CSS <- ".shiny-output-error { visibility: hidden; }
+.shiny-output-error:before {
+    visibility: visible;
+    content: 'Check that your uploaded file is formatted properly'; }"
 
 # UI #
 ui <- dashboardPage(skin = "black", title = "Catapult",
@@ -25,7 +32,7 @@ ui <- dashboardPage(skin = "black", title = "Catapult",
         ),
                     
         ## BODY ##
-        dashboardBody(
+        dashboardBody(inlineCSS(CSS),
             tabItems(
                 
                 ## TAB 1 - Import File ##
@@ -57,22 +64,43 @@ ui <- dashboardPage(skin = "black", title = "Catapult",
                     )
                 ),
                 
-                ## TAB 2 -  ##
+                ## TAB 2 - Team ##
                 tabItem(tabName = "tab2",
                         
                     ## ROW 1 ##
                     fluidRow(
                         column(5,
                             h3(strong(""))
-                        ),
+                        )
+                    ),
                         
                     ## ROW 2 ##
                     fluidRow(
                         column(12,
                                withSpinner(plotlyOutput("AverageTeamLoad"), type = 7, color = "#FFCC00", size = 2) 
                         )
+                    ),
+                    br(),
+                    br(),
+                        
+                    ## ROW 3 ##
+                    fluidRow(
+                        column(12,
+                               withSpinner(plotlyOutput("AverageGameCodeLoad"), type = 7, color = "#FFCC00", size = 2) 
+                        )
                     )
-                    )
+                ),
+                
+                ## TAB 3 - Player ##
+                tabItem(tabName = "tab3",
+                        
+                    ## ROW 1 ##
+                    fluidRow(
+                        column(5,
+                               h3(strong(""))
+                        )
+                    )        
+                    
                 )
             )
         )
@@ -165,8 +193,20 @@ server <- function(input, output) {
         p1 <- Data() %>% select(playerLoad, Date, Activity, gameCode) %>% group_by(Date) %>% 
             mutate(averagePlayerLoad = mean(playerLoad)) %>% distinct(Date, .keep_all = TRUE) %>% select(-playerLoad) %>%
             ggplot(aes(x = Date, y = averagePlayerLoad, group = 1)) + geom_point(aes(color = Activity), size = 4) + geom_line() + theme_bw() +
-            theme(axis.text.x = element_text(angle = 45, hjust = 1))
+            theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5)) + 
+            scale_color_manual(values = c("#FFCC00", "#003366")) + ggtitle("Average Player Load by Date")
         ggplotly(p1)
+    })
+    
+    # Plotly Average Player Load/gameCode
+    output$AverageGameCodeLoad <- renderPlotly({
+        p2 <- Data() %>% select(playerLoad, Activity, gameCode) %>% filter(gameCode %in% c("G","G-1","G-2","G-3","G-4","G-5","G-6","G-7")) %>% 
+            mutate(gameCode = factor(gameCode, levels = c("G-7","G-6","G-5","G-4","G-3","G-2","G-1","G"))) %>%
+            group_by(gameCode) %>% mutate(averagePlayerLoad = mean(playerLoad)) %>% select(-playerLoad) %>% 
+            ggplot(aes(x = gameCode, y = averagePlayerLoad, group = 1)) + geom_point(aes(color = Activity), size = 4) + geom_line() + 
+            theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5)) + 
+            scale_color_manual(values = c("#FFCC00", "#003366")) + ggtitle("Average Player Load by Game Code")
+        ggplotly(p2)
     })
 }
 
