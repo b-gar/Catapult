06fef4f5a0809ae2db9d4c5fe4c48e1eba46ca4f
Ben Garski <blgarski@uwm.edu>
2020-03-31 16:15:56 -0500

    Formatting/Rounded ACWR

--- movingAverages.R
+++ movingAverages.R
@@ -1,62 +1,61 @@
 library(TTR)
+library(reshape2)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(tidyquant)
 
 # EMA: lambda*valueToday + 1-lambda * EWMABefore
 # lambda = 2/n+1
 
-## EWMA by Team Sum ##
+### EWMA - Team ###
 
 # Read Data
 df <- read.csv("catapultValues.csv")
 df$X <- NULL
 
 # Team EWMA by Summation of Player Load Using TTR Package
-test <- df %>% group_by(Date) %>% summarise(playerLoad = sum(playerLoad)) %>% 
+team <- df %>% group_by(Date) %>% summarise(playerLoad = sum(playerLoad)) %>% 
   mutate(Acute = EMA(playerLoad, 7), Chronic = EMA(playerLoad, 28))
 
 # Turn DF Long to Plot in ggplot2
-library(reshape2)
-
-testLong <- test %>% melt() %>% transmute(Date= as.Date(Date), Variable = variable, Value = value)
+teamLong <- team %>% melt() %>% transmute(Date= as.Date(Date), Variable = variable, Value = value)
 
 # Plot
-g <- ggplot(testLong, aes(Date, Value, color = Variable)) + geom_line(group = 1, alpha = 0.9) + 
+g <- ggplot(teamLong, aes(Date, Value, color = Variable)) + geom_line(group = 1, alpha = 0.9) + 
   scale_color_manual(name = "", values = c("black", "#1b9e77", "#7570b3")) + 
   ggtitle("Player Load Acute/Chronic") + xlab("") + ylab("Player Load") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
 ggplotly(g) #%>% api_create(filename = "Acute vs. Chronic Player Load")
 
 # Easier - Using Tidyqant
 # Can't Use Plotly with geom_ma so Above Would Need to be Used
-library(tidyquant)
 
-test$Date <- as.Date(test$Date)
-ggplot(test, aes(Date, playerLoad)) + geom_line(aes(color = "Values")) + 
+team$Date <- as.Date(team$Date)
+ggplot(team, aes(Date, playerLoad)) + geom_line(aes(color = "Values")) + 
   geom_ma(aes(color = "Acute"), ma_fun = EMA, n = 7, show.legend = TRUE, linetype = 1, size = 1) + 
   geom_ma(aes(color = "Chronic"), ma_fun = EMA, n = 28, show.legend = TRUE, linetype = 1, size = 1) +
   scale_color_manual(name = "", breaks = c("Values", "Acute", "Chronic"), 
                      values = c("Values" = "black", "Acute" = "#1b9e77", "Chronic" = "#7570b3")) + 
   ggtitle("Player Load Acute/Chronic") + xlab("") + ylab("Player Load") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
 
-## EWMA for a Player ##
-test <- df %>% filter(Name == Name[2]) %>% select(Date, playerLoad) %>%
+### EWMA - Player ###
+player <- df %>% filter(Name == Name[2]) %>% select(Date, playerLoad) %>%
   mutate(Acute = EMA(playerLoad, 7), Chronic = EMA(playerLoad, 28))
 
-testLong <- test %>% melt() %>% transmute(Date= as.Date(Date), Variable = variable, Value = value)
+playerLong <- player %>% melt() %>% transmute(Date= as.Date(Date), Variable = variable, Value = value)
 
-# Plot Including playerLoad
-g <- ggplot(testLong, aes(Date, Value, color = Variable)) + geom_line(group = 1, alpha = 0.9) + 
+# Plot
+g2 <- ggplot(playerLong, aes(Date, Value, color = Variable)) + geom_line(group = 1, alpha = 0.9) + 
   scale_color_manual(name = "", values = c("black", "#1b9e77", "#7570b3")) + 
   ggtitle("Player Load Acute/Chronic") + xlab("") + ylab("Player Load") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
-ggplotly(g) #%>% api_create(filename = "Acute vs. Chronic Player Load")
+ggplotly(g2) #%>% api_create(filename = "Acute vs. Chronic Player Load")
 
 # ACWR w/o playerLoad
-test <- df %>% filter(Name == Name[2]) %>% select(Date, playerLoad) %>%
-  mutate(Acute = EMA(playerLoad, 7), Chronic = EMA(playerLoad, 28), ACWR = Acute/Chronic) %>%
+acwr <- df %>% filter(Name == Name[2]) %>% select(Date, playerLoad) %>%
+  mutate(Acute = EMA(playerLoad, 7), Chronic = EMA(playerLoad, 28), ACWR = round(Acute/Chronic, 3)) %>%
   transmute(Date = as.Date(Date), ACWR = ACWR)
 
 # Plot ACWR Only
-g <- ggplot(test, aes(Date, ACWR)) + geom_line(group=1) + scale_y_continuous(breaks = seq(0,2,0.5), limits = c(0,2)) +
+g3 <- ggplot(acwr, aes(Date, ACWR)) + geom_line(group=1) + scale_y_continuous(breaks = seq(0,2,0.5), limits = c(0,2)) +
   ggtitle("Player Acute/Chronic Workload Ratio") + xlab("") + ylab("ACWR") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
-ggplotly(g) #%>% api_create(filename = "ACWR")
+ggplotly(g3) #%>% api_create(filename = "ACWR")
