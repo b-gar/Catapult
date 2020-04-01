de9ae188c438c682657777ab5342d30f518c3509
Ben Garski <blgarski@uwm.edu>
2020-04-01 14:46:46 -0500

    hard-coded y-scale ACWR

--- app.R
+++ app.R
@@ -335,7 +335,7 @@ server <- function(input, output, session) {
       p7 <- Data() %>% filter(Name == input$player) %>%
         mutate(Acute = EMA(playerLoad, 7), Chronic = EMA(playerLoad, 28), ACWR = round(Acute/Chronic, 3)) %>%
         ggplot(aes(Date, ACWR, text = paste0("Date: ", Date, "\n", "Acute: ", round(Acute, 3), "\n", "Chronic: ", round(Chronic, 3), "\n", "ACWR: ", ACWR))) + 
-        geom_line(group = 1) + 
+        geom_line(group = 1) + scale_y_continuous(breaks = seq(0.5,1.5,0.5), limits = c(0.5,1.5)) +
         ggtitle("Player Acute/Chronic Workload Ratio") + xlab("") + ylab("ACWR") + theme_bw() + 
         theme(plot.title = element_text(hjust = 0.5))
       ggplotly(p7, tooltip = "text") %>% config(displayModeBar = FALSE)
