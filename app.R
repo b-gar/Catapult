83813ae3ca274135f1dfc4733694a7a814e7ab73
Ben Garski <blgarski@uwm.edu>
2020-04-06 14:59:43 -0500

    Added acwr value to notification

--- app.R
+++ app.R
@@ -247,8 +247,8 @@ server <- function(input, output, session) {
       }
       if (nrow(allPlayers) > 0) {
         msgs <- allPlayers %>% 
-          mutate(WarningMessage = case_when(ACWR < input$acwr[1] ~ paste(Name, "has a low ACWR on", Date), 
-                                            ACWR > input$acwr[2] ~ paste(Name, "has a high ACWR on", Date)
+          mutate(WarningMessage = case_when(ACWR < input$acwr[1] ~ paste(Name, "has a low ACWR of", ACWR, "on", Date), 
+                                            ACWR > input$acwr[2] ~ paste(Name, "has a high ACWR of", ACWR, "on", Date)
                                             )
           ) %>%
           mutate(Warning = case_when(ACWR < input$acwr[1] ~ "Low", 
