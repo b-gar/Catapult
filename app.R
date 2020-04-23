fbbff9aea00f5d483728505d5bc17399e8a5aeb6
Ben Garski <blgarski@uwm.edu>
2020-04-23 14:09:42 -0500

    Removed notificationItem when not needed

--- app.R
+++ app.R
@@ -277,7 +277,6 @@ server <- function(input, output, session) {
       }
       else(
         dropdownMenu(
-          notificationItem(text = "There are no high ACWR values", icon = icon("thumbs-up")), 
           badgeStatus = "success", icon = icon("arrow-up", lib = "glyphicon")
         )
       )
@@ -296,7 +295,6 @@ server <- function(input, output, session) {
       }
       else(
         dropdownMenu(
-          notificationItem(text = "There are no low ACWR values", icon = icon("thumbs-up")), 
           badgeStatus = "success", icon = icon("arrow-down", lib = "glyphicon")
         )
       )
@@ -317,7 +315,6 @@ server <- function(input, output, session) {
       }
       else(
         dropdownMenu(
-          notificationItem(text = "No max velocity errors detected", icon = icon("thumbs-up")), 
           badgeStatus = "success", icon = icon("tachometer-alt")
         )
       )
@@ -338,8 +335,7 @@ server <- function(input, output, session) {
       }
       else(
         dropdownMenu(
-          notificationItem(text = "No GPS errors detected", icon = icon("thumbs-up")), badgeStatus = "success",
-          icon = icon("satellite")
+          badgeStatus = "success", icon = icon("fas fa-satellite-dish")
         )
       )
     })
